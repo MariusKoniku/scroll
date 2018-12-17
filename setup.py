29221728d5bbb04204f27f458099e709d83c4bfa
marius <guerard@koniku.com>
2018-12-17 14:42:34 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'scroll',
+    version='0.1.0',
+    #description='Sample package for Python-Guide.org',
+    #long_description=readme,
+    author='Marius Guerard',
+    author_email='guerard@koniku.com',
+    url='https://github.com/MariusKoniku/scroll/',
+    #license=license,
+    #packages=find_packages(exclude=('tests', 'docs'))
+)
